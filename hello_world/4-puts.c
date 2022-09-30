49c94cdb6f6abdeb9c45584e83db499543d0e164
uoch <chaouch.thameurtc61@gmail.com>
2022-09-30 01:34:31 -0700

    assembler

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+ 
+  // initializing strings
+  char str1[] = "Programming is like building a multilingual puzzle;
+
+  // writing to stdout
+  puts(str1);
+
+  return 0;
+}
