c4004525925407c8890b6a26aaeed31ad804d88e
uoch <chaouch.thameurtc61@gmail.com>
2022-09-30 03:51:22 -0700

    size bit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@ char charType;
 long int x;
 long long int y;
 /* sizeof evaluates the size of a variable*/
-printf("Size a char: %zu byte\n", sizeof(charType));
-printf("Size an int: %zu bytes\n", sizeof(intType));
-printf("Size a long int: %zu bytes\n", sizeof(x));
-printf("Size a long long int: %zu bytes\n", sizeof(y));
-printf("Size a float: %zu bytes\n", sizeof(floatType));
+printf("Size of  a char: %zu byte\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(x));
+printf("Size of a long long int: %zu bytes\n", sizeof(y));
+printf("Size of a float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
 
