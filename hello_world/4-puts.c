25c82420e2322279bc62c36d8ab892d0ab3caa55
uoch <chaouch.thameurtc61@gmail.com>
2022-09-30 02:13:16 -0700

    fuck betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+/* welcom to betty */
 int main(void)
+/* welcom to betty */
 {
 /* initializing strings */
 char str1[] = "\"Programming is like building a multilingual puzzle";
