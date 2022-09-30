95ec6f22ba244bee58e37a943ff7fe6728eef5d8
uoch <chaouch.thameurtc61@gmail.com>
2022-09-30 01:53:42 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(void) {char str1[] = "\"Programming is like building a multilingual puzzle";
+int main(void)
+{
+/* initializing strings */
+char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
 }
