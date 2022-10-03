fb76bae85fc3da92c110003c1b486bd0e5d51d96
uoch <chaouch.thameurtc61@gmail.com>
2022-10-03 05:28:36 -0700

    alpha

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,10 @@
 int main(void)
 {
         int i = 97;
-while ( i <= 122 )
-{
-    if (i != 113 && i != 101)
-    {
+while ( i <= 122 && (i != 113 && i != 101))
+{   
         putchar(i);
     i++;
-    }
 }
         putchar('\n');
 return (0);
