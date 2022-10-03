1b72f0c69f0bce3d8da44b392491b21d0861ca2f
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-03 22:52:16 +0200

    num value + -

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
     /* your code goes there */
@@ -16,6 +16,6 @@ int main(void)
 	if (n == 0)
 		printf("%d is zero\n", n);
 	if (n < 0)
-    	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 return (0);
 }
