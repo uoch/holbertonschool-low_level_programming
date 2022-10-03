7bd4e3d01a6db107011f41c36f4f7e9f92166a81
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-03 23:35:03 +0200

    ascii numb

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-    int i = 48;
-    while (i <= 57)
-    {
-        putchar(i);
-        i++;
-    }
-    putchar('\n');
-    return (0);
+int i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
 }
