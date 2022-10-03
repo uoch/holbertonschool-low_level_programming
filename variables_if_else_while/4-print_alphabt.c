655d0e4fd43a252264d124926c82f26e7453d0d8
uoch <chaouch.thameurtc61@gmail.com>
2022-10-03 05:24:20 -0700

    alpha

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+        int i = 97;
+while ( i <= 122 )
+{
+    if (i != 113 && i != 101)
+    {
+        putchar(i);
+    i++;
+    }
+}
+        putchar('\n');
+return (0);
+}
