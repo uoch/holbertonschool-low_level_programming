496cc592ef43dce352e6e678dcaa6837dadeaf6e
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-03 22:58:16 +0200

    none q,e)

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-        int i = 97;
+int i = 97;
 while ( i <= 122 )
 {   
-        if (i != 113 && i != 101)
-        putchar(i);
-    i++;
+if (i != 113 && i != 101)
+putchar(i);
+i++;
 }
-        putchar('\n');
+putchar('\n');
 return (0);
 }
