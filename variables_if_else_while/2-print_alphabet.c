20c9ddb50fbca703d629ab07d74a612e607a61b1
uoch <chaouch.thameurtc61@gmail.com>
2022-10-03 04:07:00 -0700

    alpha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 97;
+while ( i <= 122 )
+{
+	putchar(i);
+    i++;
+}
+	putchar('\n');
+return (0);
+}
