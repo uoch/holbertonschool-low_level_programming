7d6d69f0caddc58e70393de910e8cc31a3e11e26
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-05 00:07:34 +0200

    test alpha exist

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,17 +8,17 @@
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
-	    {
-            return (1);
-        }
+	{
+        return (1);
+    }
     else if (c >= 65 && c <= 90)
-        {
-		    return (1);
-	    }
-	else
-	    {
-		    return (0);
-	    }
-	_putchar(10);
-	return (0);
+    {
+		return (1);
+	}
+    else
+	{
+    return (0);
+	}
+    _putchar(10);
+    return (0);
 }
