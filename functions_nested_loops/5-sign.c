1aa942b1af5e07a3cf4070dfe2e64c0e86f8d928
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-05 00:11:49 +0200

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - let us know if a given value is less or great than 0.
+ * @n: value given.
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar(10);
+	return (0);
+}
