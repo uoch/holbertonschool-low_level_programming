6653070629141bdf78688b8962814731852a94e3
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-04 22:01:47 +0200

    num with function

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,10 +1,11 @@
 #include "main.h"
-
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+void print_alphabet(void);
+
 int main(void)
 {
 	print_alphabet();
