38bc3b1a3494091b2709b08a9cf8272794395439
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-04 21:51:40 +0200

    num with function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+ls#include <stdio.h>
 /**
  * main - Entry point
+ @c: The character to print
  * Return: 0
  */
+ int _putchar(char c);
+
 void print_alphabet_x10(void)
 {
         int i = 97;
 while (i <= 122)
 {
-        putchar(i);
+        _putchar(i);
         i++;
 }
-        putchar('\n');
+        _putchar('\n');
 return (0);
 }
