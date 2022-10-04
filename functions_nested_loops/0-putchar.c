61196526cf0962ddf83536128b46798996bfa193
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-04 21:44:40 +0200

    main

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - check the code
+ * Return: 0
  */
-int _putchar(char c)
+int _putchar(char c);
+int main(void)
+
 {
-	return (write(1, &c, 1));
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
 }
