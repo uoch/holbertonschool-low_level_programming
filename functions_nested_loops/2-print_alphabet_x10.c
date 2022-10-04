5ad995c093ffc622e17e38239650813cc9ba1556
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-04 22:37:43 +0200

    num with function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ @ alpha : game
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+    int j
+    for (j=0; i<=9; j++)
+        {
+            int i = 97;
+            while (i <= 122)
+                {
+                    _putchar(i);
+                    i++;
+                }
+        }
+    _putchar(10);
+}
