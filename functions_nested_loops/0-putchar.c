84ad48cc43941339166948a14435f454de31a983
uoch <chaouch.thameurtc61@gmail.com>
2022-10-04 03:34:21 -0700

    main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
  * Return: 0
@@ -6,14 +6,6 @@
 
 int main(void)
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-	_putchar(10);
+	printf("_putchar\n");
 	return (0);
 }
