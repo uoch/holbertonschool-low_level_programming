69ac7156822e6dda41c7ab82353051d25cc672fe
uoch <chaouch.thameurtc61@gmail.com>
2022-10-05 06:27:18 -0700

    9*9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,14 +5,18 @@
  */
 void times_table(void)
 {
-	int i, j
+	int a, b, c;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
 		{
-			_putchar( i * j);
+			_putchar(44);
+			_putchar(32);
+
+			c = a * b;
 		}
-	}
 	_putchar(10);
 }
