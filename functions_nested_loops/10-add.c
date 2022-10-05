0f6ba883a8f2c7e602b8197aee9d51fbe59edeb6
uoch <chaouch.thameurtc61@gmail.com>
2022-10-05 06:42:22 -0700

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - present the sum of two given variables.
+ * @x: first argument.
+ * @y: second argument.
+ *
+ * Return: sum of the value of the given variables.
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
