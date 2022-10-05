da38408f17851607a4e26537f60f7a1d15d2ed97
uoch <chaouch.thameurtc61@gmail.com>
2022-10-04 23:59:52 -0700

    jack

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+ {
+ 	 int i .j ;
+	 i = 0 ;
+	 j = 0;
+	 for (i < 24 ; i++)
+	 {
+	 	for (j < 60 ; j++)
+		{
+		if (j < 10 || i<10)
+			_putchar('0'i':''0'j);
+		else
+		_putchar(i':'j);
+		}		
+	 }
+ }
+
