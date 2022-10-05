a7d67185219c2100adbc920d5b80061a361d24db
uoch <chaouch.thameurtc61@gmail.com>
2022-10-05 04:22:40 -0700

    times

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * times_table - prints an n times table.
+ *
+ */
+void times_table(void)
+{
+	int i, j
+	for (i = 0; j <= 9; a++)
+	{
+		for (j = 0; j <= 9; b++)
+		{
+			_putchar( i * j)
+		}
+	}
+	_putchar(10);
+}
