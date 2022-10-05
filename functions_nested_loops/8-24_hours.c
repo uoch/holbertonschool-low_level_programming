8b27363c87a304d3265b492665c3049d2d371e7c
uoch <chaouch.thameurtc61@gmail.com>
2022-10-05 04:14:04 -0700

    clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,25 +1,21 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day, 24 hour clock
- * h = hour, m = minutes
- * / 10 allows second digit to rotate
- * for loop breaks before passing 24:00
- * Return: 24 hour clock line by line
+ * jack_bauer - display 24 hours per hours and minutes.
  */
 void jack_bauer(void)
 {
-	int h, m;
+	int a, b;
 
-	for (h = ; h < 24; h++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (m = 0; m < 60; m++)
+		for (b = 0; b <= 59; b++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(58);
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(10);
 		}
 	}
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,4 +7,4 @@ int _islower(int c);
 int _abs(int);
 int print_last_digit(int);
 int print_sign(int n);
-void jack_bauer(void)
+void jack_bauer(void);
