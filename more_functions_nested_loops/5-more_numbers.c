169d6cd7f2671ee06bf1dbca9299d8464199a497
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-05 23:48:02 +0200

    mullt

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 01234567891011121314\n x10
+ *
+ * Return: void
+ */
+
+
+void nmubb014(void)
+void more_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '14'; c++)
+	{
+		nmubb014();
+	}
+	_putchar(10);
+    
+}
