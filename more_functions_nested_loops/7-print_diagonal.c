d0f9e11619402ae079b611c834902909586b22d9
uoch <chaouch.thameurtc61@gmail.com>
2022-10-06 05:16:09 -0700

    line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,20 @@
 
 
 void print_diagonal(int n)
+{
+	int a , b;
+	if (n <= 0)
 	{
-		int a , b;
-		if (n <= 0)
-			{
-				_putchar(10);
+		_putchar(10);
 			}
-		for (a = 0 ; a <= n ; a++)
+	for (a = 0 ; a <= n ; a++)
+	{
+		for ( b = 0; b <= a; b++)
 		{
-			for ( b = 0; b <= a; b++)
-			{
-				_putchar(' ');
-				_putchar('\\');
-				_putchar(10);
-			}
+			_putchar(' ');
+			_putchar('\\');
 			_putchar(10);
 		}
+		_putchar(10);
 	}
+}
