a5bb972de7d0d2b9df91d02bb1a63781946d6444
uoch <chaouch.thameurtc61@gmail.com>
2022-10-06 08:41:16 -0700

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from any integer to 98
+ * @n: integer input
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		d = n / 10;
+		e = n % 10;
+		_putchar(d);
+		_putchar(e);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar(10);
+}
