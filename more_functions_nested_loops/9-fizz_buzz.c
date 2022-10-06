ca4f7e83f1b14f7c2a2abd73ff4c4e26f47f6cd7
uoch <chaouch.thameurtc61@gmail.com>
2022-10-06 05:43:59 -0700

    #

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			_putchar('FizzBuzz');
+		else if (i % 3 == 0)
+			_putchar('Fizz');
+		else if (i % 5 == 0)
+			printf('Buzz');
+		else
+			_putchar(i);
+		if (i < 100)
+			_putchar(' ');
+	}
+	_putchar(10);
+	return (0);
+}
