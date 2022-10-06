f7b8a9dd70821c4ad004c91bfa6edf3f212df061
uoch <chaouch.thameurtc61@gmail.com>
2022-10-06 05:57:23 -0700

    #

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal line of length n
- * @n: length of diagonal
+ * print_triangle - prints a triangle of size size
+ * @size: size of triangle
  *
  * Return: void
  */
 
 
-void print_diagonal(int n)
+void print_triangle(int size)
 {
-        int a , b;
-        if (n <= 0)
-        {
-                _putchar(10);
-                        }
-        for (a = 0 ; a < n ; a++)
-        {
-                for ( b = 0; b < a; b++)
-                {
-                        _putchar('#');
-                }
-                _putchar(10);
-        }
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b <= (size - a))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+
 }
