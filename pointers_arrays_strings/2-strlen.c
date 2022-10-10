a22d9815f20096960100c3e72a158f0d8639badf
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 19:59:38 +0200

    pointer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of int a and int b
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+    
+    while ( s[i] != '\0')
+        i++;
+    return(i);
+
+}
