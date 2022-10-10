e7cc87d28761d7c6dfe623718b058adc0eacc6a9
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 22:25:54 +0200

    n,

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,10 @@
  */
 void print_array(int *a, int n)
 {
-    for (int i = 0; i < n; i++)
+    int i = 0;
+    while(*(a + i))
     {
-        _putchar(*(a+i));
+        _putchar('*(a+i)');
         _putchar(',');
         _putchar(10);
     }
