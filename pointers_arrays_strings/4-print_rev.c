d0becfdbd859c546cc9c6412473913e59abd7b02
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 20:57:38 +0200

    pointer

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in stdout
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0')
+        i++;
+    while (i = j)
+    {
+        j = i-1;
+        _putchar(*(str + j));
+		j++;
+    }
+    
+}
