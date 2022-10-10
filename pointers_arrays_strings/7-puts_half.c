7d91fa9568b429654fdae28b8318c842cd743398
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 22:09:53 +0200

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a  string to stdout
+ * puts_half - prints the second half of a string
  * @str: string to print
  *
  * Return: void
  */
 void puts_half(char *str)
 {
-    	int i = 0, j = 0;
+	int j, i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i))
 		i++;
-    j = i/2;
-    while (j != i)
-    {
-        _putchar(*(str + i));
-        j++;
-    }
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	while (j < i)
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	_putchar('\n');
 }
