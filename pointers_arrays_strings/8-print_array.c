64dfd063dec74b2b69e896ea264dce10447b119e
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 22:30:31 +0200

    n,

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * puts_half - prints the second half of a string
- * @str: string to print
+ * print_array - prints n elements in the array a
+ * @a: array to print
+ * @n: number of elements to print
  *
  * Return: void
  */
 void print_array(int *a, int n)
 {
-    int i = 0;
-    while(*(a + i))
-    {
-        _putchar('*(a+i)');
-        _putchar(',');
-        _putchar(10);
-    }
-    
-}
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
