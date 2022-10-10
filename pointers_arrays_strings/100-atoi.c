7c8c41139968d587457547ca91c9b633395e8994
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-10 23:55:12 +0200

    copy-paste

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: value of integer
+ */
 int _atoi(char *s)
 {
-    int i = 0; 
-    int n = 0;
-    while (*(s + i))
-    {
-        if ((*(s + i) == '+') || (*(s + i) == '-'))
-        if ((*(s + ( i + 1)) >= '0') && (*(s + ( i + 1)) <= '9')) 
-        i++;
-    }
-    n = i + 1;
-    return (n);
+	int i, j, n, m;
+
+	i = n = 0;
+	m = 1;
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	j = i;
+	while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+	{
+		n = n * 10 + m * (*(s + j) - '0');
+		j++;
+	}
+	return (n);
 }
