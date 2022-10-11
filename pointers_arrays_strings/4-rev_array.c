b774602f28de9d91b51ac7a9237aa9600860622e
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-11 21:24:43 +0200

    rev

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
+/**
+ * reverse_array - reverses the contents of an array
+ * @a: integer array to be reversed
+ * @n: length of array a
+ *
+ * Return: void
+ */
+
 void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	i = 0; 
+	i = 0;
 	j = 0;
 	while (*(a + i))
 		i++;
 	while (i >= 0)
 	{
 		n = *(a + i);
-		*(a + i) = ;
-		*(a + j) = n
+		*(a + i) = *(a + j);
+		*(a + j) = n;
 		i--;
 		j++;
 	}
-	*(a + j) = n;
 }
