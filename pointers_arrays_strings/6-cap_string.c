5cd2187397504205e7d86f6942c3e9c49807a4e6
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-11 22:58:52 +0200

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,30 @@
  */
 char *cap_string(char *s)
 {
-    int i = 0, j = 0;
-    char a[] = " \t\n,;.!?\"(){}";
-    int n = 0;
-    while (*a + j)
-    {
-        j++;
-    }
-    while (*(s + i))
-    {
+	int i = 0, j = 0;
+	char a[] = " \t\n,;.!?\"(){}";
+	int n = 0;
+	while (*a + j)
+	{
+		j++;
+	}
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
 
-        for (n = 0; n <= j; n++)
-        {
-            if (a[j] == *(s + i - 1))
-                *(s + i) -= 'a' - 'A';
-        }
-		i++;
-    }
-	return (s);
+				for (n = 0; n <= j; n++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+				i++;
+			}
+			return (s);
+		}
+	}
 }
