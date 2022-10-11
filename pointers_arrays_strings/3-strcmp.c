ecbcaa66cb30b0594be4d050681843afb3861314
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-11 20:41:07 +0200

    1-s

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i, j, n;
-
-    i = j = 0;
-    while (*(s1 + i) || *(s2 + j))
-    {
-        n = (*(s1 + i)) - (*(s2 + j));
-        j++;
-        i++;
-    }
-    return (n);
+	int i ;
+	i = 0;
+	while (*(s1 + i) || *(s2 + i))
+	{
+		n = (*(s1 + i)) - (*(s2 + j));
+		j++;
+	}
+	return (n);
 }
