cdb85e395e8af2229c14201102253ec172fb4db7
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-11 20:02:40 +0200

    1-s

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ *
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i, j, n;
+
+    i = j = 0;
+    while (*(dest + i))
+        i++;
+    while ((*(dest + i) = *(src + j)) || (j <= n))
+    {
+        i++;
+        j++;
+    }
+    return (dest);
+}
