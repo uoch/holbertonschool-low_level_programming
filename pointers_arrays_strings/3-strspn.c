82eaa7dc7cdaf37b3c8e2be6032bbd1e6734d5e8
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-12 20:59:11 +0200

    look

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int j;
+	unsigned int i, j;
 
-	unsigned int _strspn(char *s, char *accept)
+	for (i = 0; *(s + i); i++)
 	{
-		unsigned int i, j;
-
-		for (i = 0; *(s + i); i++)
+		for (j = 0; *(accept + j); j++)
 		{
-			for (j = 0; *(accept + j); j++)
-			{
-				if (*(s + i) == *(accept + j))
-					break;
-			}
-			if (!accept[j])
+			if (*(s + i) == *(accept + j))
 				break;
 		}
-		return (i);
+		if (!accept[j])
+			break;
 	}
+	return (i);
+}
