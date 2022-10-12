fd132921ae16ff00cb582f0a5c050a947000099f
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-12 21:20:48 +0200

    look

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,23 @@
  *
  * Return: number of bytes of s in accept
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0; *(s + i); i++)
+	while (*(s + i))
 	{
-		for (j = 0; *(accept + j); j++)
+		while (*(accept + j))
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
 				break;
+
+			if (!accept[j])
+				break;
+			j++;
 		}
+		i++;
 	}
 	return (i);
 }
