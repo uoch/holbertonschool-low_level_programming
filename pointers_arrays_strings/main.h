123e4f1a45659494b83790a1e5f8fe2ad4a57d8b
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-12 20:07:00 +0200

    fil

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *s);
+char *leet(char *s);
+char *_memset(char *s, char b, unsigned int n);
