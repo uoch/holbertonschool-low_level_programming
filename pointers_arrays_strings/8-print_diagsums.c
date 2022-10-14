c1137b01c60af093066dc67a00962e1fc7986800
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-14 13:04:06 +0200

    search acsept

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,16 +3,16 @@
 #include "main.h"
 void print_diagsums(int *a, int size)
 {
-	int i, j, n, k, g ;
+	int i, j, n, k, g;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				k += *(a + j) + j;
+				k += *(a + i) + i;
 			n = size - j;
-			g += *(a + n) + j;
+			g += *(a + j) + n;
 		}
 	}
 	printf("%d ,%d\n", k, g);
