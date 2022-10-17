c43dab77dc01fb0aeb22d55d47039dad31fcc190
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-17 22:19:50 +0200

    put char count

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
+ * @c: number to find sqrt of
+ *
+ * Return: -1 or sqrt of c
+ */
+int _sqrt_recursion(int n)
+{
+    int x = 1;
+
+
+	if (n == 0)
+		return (0);
+
+    if (n == _pow_recursion( (x+1) , 2))
+    return (x);
+
+
+
+
+}
+    
