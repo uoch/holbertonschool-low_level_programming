4c3fa1be3d60e2bbb20958eea27bbec52f6ee32c
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-17 21:31:26 +0200

    put char count

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+    int n;
+
+    if (*s == '\0')
+        return (0);
+    n = 1 + _strlen_recursion(s + 1) return (n);
+}
