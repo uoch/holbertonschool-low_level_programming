6d6483b3200cca9f27f0418607f8229377ae7a3b
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-17 21:33:18 +0200

    put char count

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-    int n;
+	int n;
 
-    if (*s == '\0')
-        return (0);
-    n = 1 + _strlen_recursion(s + 1); 
-    return (n);
+	if (*s == '\0')
+		return (0);
+	n = 1 + _strlen_recursion(s + 1);
+	return (n);
 }
