1b2e9b4da603192af57769522227236faba4b3e4
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-19 22:15:35 +0200

    ar

---
+++ static_libraries/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - check for lowercase characters
+ * @c: character check
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+        return (1);
+    }
+    else if (c >= 65 && c <= 90)
+    {
+		return (1);
+	}
+    else
+	{
+    return (0);
+	}
+    _putchar(10);
+    return (0);
+}
