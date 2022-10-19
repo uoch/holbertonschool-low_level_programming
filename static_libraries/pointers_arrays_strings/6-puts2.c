693b8cb2d11f9bbe8d4c7173bc7075022cbf3379
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-19 22:28:54 +0200

    put char count

---
+++ static_libraries/pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
