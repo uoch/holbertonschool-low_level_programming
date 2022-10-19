82b80420cd83f41385321f6052fd2dddca869912
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-19 22:35:52 +0200

    ar

---
+++ static_libraries/pointers_arrays_strings/3-strspn copy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets lengthof a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: number of bytes of s in accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (*(s + i))
+	{
+		while (accept[j] && (s[i] == accept[j]))
+		{
+			j++;
+		}
+		i++;
+	}
+	return (j);
+}
