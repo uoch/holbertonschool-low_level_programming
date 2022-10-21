dd2e45ce4a940a59f550e2265067b9bf8f1b8c5b
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-21 20:58:18 +0200

    argc

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,9 +7,8 @@
  *
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-    printf("%d\n", argc - 1);
-    (void)argv[];
-    return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
