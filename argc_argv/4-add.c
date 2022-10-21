f6f5e2f48ad534ccebfb2f670f4a01c56d358192
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-21 21:12:00 +0200

    argc

---
+++ argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, ;
+
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
