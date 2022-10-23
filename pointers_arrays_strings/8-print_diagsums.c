b26146392e2c92e5972894fb130f1564ed66b09c
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-23 18:57:21 +0200

    rot 13

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,19 @@
 
 #include <stdio.h>
 #include "main.h"
-void print_diagsums(int *a, int size)
+void print_diagsums(int (*a)[size], int size)
 {
-	int i, j, n, k, g;
+	int i, j, k, g;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				k += *(a + i) + i;
-			n = size - j;
-			g += *(a + j) + n;
+			{
+				k += a[i][j];
+				g += a[i][size - j];
+			}
 		}
 	}
 	printf("%d ,%d\n", k, g);
