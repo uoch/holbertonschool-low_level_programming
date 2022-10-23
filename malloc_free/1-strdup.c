21d26a051f315e37ec5c846a2c400e348b56791f
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-23 10:04:38 +0200

    melloc strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy a string to a newly allocated space in memory
+ * @str: string to copy
+ *
+ * Return: pointer to duplicated string or NULL if error
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int i, j;
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i))
+	{
+		i++;
+	}
+	a = malloc(i * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		*(a + j) = *(str + j);
+	}
+	return (a);
+}
