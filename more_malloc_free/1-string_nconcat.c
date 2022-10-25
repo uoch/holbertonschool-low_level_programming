d50f225d1b4c32d51e339e1f7be6c92ad06e0721
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-25 19:46:45 +0200

    concanate

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
+ *
+ * Return: char pointer to newly allocated place in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	int i = 0;
+	int j = 0;
+	int k;
+	
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		while (*(s1 + i))
+		{
+			i++;
+		}
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		while (*(s2 + j))
+		{
+			j++;
+		}
+	}
+	s = malloc((i + j + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
+}
