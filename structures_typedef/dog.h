fa21c6a72ce9501e0a3c868ccbda33bd08d1aa9e
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-31 19:38:55 +0100

    free dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -28,7 +28,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strncpy(char *dest, char *src, int n);
+char *_strcopy(char *dest, char *src)
 int _strlen(char *s);
 void free_dog(dog_t *d);
 #endif
---
+++ structures_typedef/strcopy.c
@@ -0,0 +1,11 @@
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
