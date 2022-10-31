3326a946fbc796c5e1c5c7018c6e218857880888
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-31 19:45:11 +0100

    free dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,36 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *doggo;
+	dog_t *doggo;
 
-    if (name == NULL || age < 0 || owner == NULL)
-        return (NULL);
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
-    doggo = malloc(sizeof(dog_t));
-    if (doggo == NULL)
-        return (NULL);
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
 
-    doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-    if (doggo->name == NULL)
-    {
-        free(doggo);
-        return (NULL);
-    }
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
 
-    doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-    if (doggo->owner == NULL)
-    {
-        free(doggo->name);
-        free(doggo);
-        return (NULL);
-    }
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
 
-    doggo->name = _strcopy(doggo->name, name);
-    doggo->age = age;
-    doggo->owner = _strcopy(doggo->owner, owner);
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
 
-    return (doggo);
+	return (doggo);
 }
