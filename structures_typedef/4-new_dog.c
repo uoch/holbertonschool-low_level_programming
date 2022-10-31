fcbdb94898999ba54cb423ea8fc2ef770424ea40
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-31 19:43:01 +0100

    free dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -27,9 +27,9 @@ dog_t *new_dog(char *name, float age, char *owner)
         return (NULL);
     }
 
-    doggo->name = _strncopy(doggo->name, name);
+    doggo->name = _strcopy(doggo->name, name);
     doggo->age = age;
-    doggo->owner = _strncopy(doggo->owner, owner);
+    doggo->owner = _strcopy(doggo->owner, owner);
 
     return (doggo);
 }
