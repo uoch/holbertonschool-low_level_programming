b6195a461216f9ceca996a38fcbbea2198d47f94
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-11-01 19:01:33 +0100

    name

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,10 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int i;
+    size_t i;
 
     if (array == NULL || action == NULL)
         return;
     for (i = 0; i <= size; i++)
-    action(*(array + i ));
+        action(*(array + i));
 }
