ade8aafd7af00115da6a0da56e897f3b09783f64
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-11-01 19:44:03 +0100

    camp

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,16 +13,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 
-    int i;
+	int i;
 
-    if (array == NULL || cmp == NULL)
-        return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
