cd17564867f266ea787f48c13a6416a72f04d3e7
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-11-02 19:42:39 +0100

    camp

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,10 +9,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc ,char *argv[])
+int main(int argc, char *argv[])
 {
-    int num1, num2;
-    char *op;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -20,25 +20,24 @@ int main(int argc ,char *argv[])
 		exit(98);
 	}
 
-    num1 = atoi(argv[1]);
-    op = argv[2];
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if (get_op_func(op) == NULL || op[1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if ((*op == '/' && num2 == 0) ||
+		(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    if ((*op == '/' && num2 == 0) ||
-        (*op == '%' && num2 == 0))
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-    printf("%d\n", get_op_func(op)(num1, num2));
-
-    return (0);
+	return (0);
 }
