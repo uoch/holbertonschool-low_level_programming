f9c2263c99fb164280a9e03bb55e71e35f61450f
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-11-02 19:40:36 +0100

    camp

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,15 +9,21 @@
  *
  * Return: Always 0.
  */
-int main(int n ,char *argv[])
+int main(int argc ,char *argv[])
 {
     int num1, num2;
     char *op;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
     num1 = atoi(argv[1]);
     op = argv[2];
     num2 = atoi(argv[3]);
-	n = 0;
+
 
     if (get_op_func(op) == NULL || op[1] != '\0')
     {
