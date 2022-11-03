011c0145a8c39033a81880eba22fa9976baa3981
uoch <chaouch.thameurtc61@gmail.com>
2022-11-03 03:53:18 -0700

    print num

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if ( (i =! (n - 1 )) && (separator != NULL))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
@@ -27,4 +27,3 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_end(nums);
 }
-
