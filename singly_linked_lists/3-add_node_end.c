803608a616f8d3141d28e2dc831d19f1c7cbeda3
uoch <chaouch.thameurtc61@gmail.com>
2022-11-14 20:12:51 +0100

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -35,5 +35,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->next = NULL;
 	*head = new;
 
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+
 	return (*head);
 }
