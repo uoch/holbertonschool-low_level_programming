58bc01372312cbaf174c20f4d6bbdc0fabdf87a8
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 19:31:03 +0100

    test

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * hare - Prints a string before the
  *        main function is executed.
  */
-void hare(void)
+void tortue() __attribute__((constructor));
+void tortue(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
