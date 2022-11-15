ab534d9558a1a95a272252de1af6873d813f2c08
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 21:48:39 +0100

    test

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list to be freed.
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t *head)
 {
     listint_t *trash;
 
