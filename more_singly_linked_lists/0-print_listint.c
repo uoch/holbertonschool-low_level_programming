07e1017ff30bb7ec64634a128b07d914409f9f70
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 19:50:37 +0100

    test

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t nb = 0;
+	size_t nb = 0;
 
-    while (h)
-    {
-        nb++;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	while (h)
+	{
+		nb++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
-    return (nb);
+	return (nb);
 }
