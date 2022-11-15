49a180a31cb46df65a676b6bf13cc9457ec3dd07
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 19:33:28 +0100

    test

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * hare - Prints a string before the
+ * tortue - Prints a string before the
  *        main function is executed.
  */
 void tortue() __attribute__((constructor));
 void tortue(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+		   "I bore my house upon my back!\n");
 }
