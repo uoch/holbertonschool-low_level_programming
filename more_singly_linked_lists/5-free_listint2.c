4a411f1b43c8f9d65fc003844658108c3acd634a
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 21:47:13 +0100

    test

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *trash;
+
+    if (head == NULL)
+        return;
+    while (head)
+    {
+        trash = head;
+        head = head->next;
+        free(trash);
+    }
+}
