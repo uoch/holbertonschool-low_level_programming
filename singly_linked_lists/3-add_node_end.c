35ed20db2c5bbcc47f08961f54295241620f79e2
uoch <chaouch.thameurtc61@gmail.com>
2022-11-15 04:45:46 -0800

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,13 +37,5 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 		*head = new;
 
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
-	}
-
 	return (*head);
 }
