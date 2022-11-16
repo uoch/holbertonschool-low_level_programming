8f4be928fd55205d48784e89cb55e4a53b4d02dd
uoch <chaouch.thameurtc61@gmail.com>
2022-11-16 20:42:57 +0100

    test

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+    listint_t *new, *copy = *head;
+    unsigned int node;
+
+    if (new == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = (*head)->next;
+        free(copy);
+        return (1);
+    }
+
+    for (node = 0; node < (index - 1); node++)
+    {
+        if (head == NULL || copy->next == NULL)
+            return (-1);
+
+        copy = copy->next;
+    }
+    copy->next = new;
+    new->next = copy->next;
+
+    return (1);
+}
