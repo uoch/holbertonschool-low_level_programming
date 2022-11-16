0577418f35987af35ac356dc5e6ed81150b7f822
uoch <chaouch.thameurtc61@gmail.com>
2022-11-16 20:55:16 +0100

    test

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
  *                           list at a given position.
  * @head: A pointer to the address of the
  *        head of the listint_t list.
- * @idx: The index of the listint_t list where the new
+ * @index: The index of the listint_t list where the new
  *       node should be added - indices start at 0.
  * @n: The integer for the new node to contain.
  *
@@ -36,6 +36,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	new = copy->next;
 	new->next = copy->next;
-
+	free(new);
 	return (1);
 }
