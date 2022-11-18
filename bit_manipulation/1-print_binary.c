27565613fe8d4d5b725b8fcf1cc17b2185583c43
uoch <chaouch.thameurtc61@gmail.com>
2022-11-18 19:49:03 +0100

    test

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,7 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned i;
-    for (i = 1 << 31; i > 0; i = i / 2)
-        (n & i) ? printf("1") : printf("0");
+	if (n > 1)
+		print_binary(n >> 1);
+
+	printf("%i",n);
 }
