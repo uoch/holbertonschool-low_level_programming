97b0ace4fab46b4706bf5dad680e6b61a97880e5
uoch <chaouch.thameurtc61@gmail.com>
2022-11-18 20:57:22 +0100

    test

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
  * Return: If an error occurs - -1.
  *         Otherwise - The value of bit at index.
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
