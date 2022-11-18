ceed34affb8b1c4daa2386d16ef568d5567ab81b
uoch <chaouch.thameurtc61@gmail.com>
2022-11-18 16:09:10 +0100

    test

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (b)
+	if (b == '/0')
 		return (0);
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
 
 		num += (b[len] - '0') * mult;
 		mult *= 2;
