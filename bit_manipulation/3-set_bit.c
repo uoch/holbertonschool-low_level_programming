d1e5c497b045664e33836235758645d84fb5f7ef
uoch <chaouch.thameurtc61@gmail.com>
2022-11-18 21:04:24 +0100

    test

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - Gets the value of a bit at a given index.
+ * set_bit - Gets the value of a bit at a given index.
  * @n: The bit.
  * @index: The index to get the value at - indices start at 0.
  *
@@ -13,5 +13,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	return (*n & 1);
+	return (*n & 0);
 }
