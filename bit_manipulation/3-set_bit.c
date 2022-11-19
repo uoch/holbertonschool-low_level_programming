c4224e90de9d7b1f23723243071642916729b337
uoch <chaouch.thameurtc61@gmail.com>
2022-11-19 00:58:52 -0800

    test

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);		
-	if( n & (1 << index) )
+	if( *n & (1 << index))
         	return (1 << index);
     	else
         	return (-1);
