a61d1b9db385548fb292f8bc9509dc0a5913088a
uoch <chaouch.thameurtc61@gmail.com>
2022-11-19 01:20:26 -0800

    test

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * clear_bit - Gets the value of a bit at a given index.
- * @n: The bit.
- * @index: The index to get the value at - indices start at 0.
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
- *         Otherwise - The value of bit at index.
+ *         Otherwise - 1.
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-    return ( nlink_t & 1);
+	*n &= ~(1 << index);
+
+	return (1);
 }
