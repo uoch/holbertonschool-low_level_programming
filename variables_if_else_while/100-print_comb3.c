6945a285365929c3c019c4fe26df69870bf1cd89
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 10:24:19 +0100

    test

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 48; i <= 57; i++)
-    {
-        for (j = 49; j <= 57; j++)
-        {
-            if (i != j)
-                putchar(i);
-            putchar(j);
-            putchar(' ');
-            putchar(',');
-        }
-    }
-    putchar(10);
-    return (0);
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (i != j)
+				putchar(i);
+			putchar(j);
+			putchar(' ');
+			putchar(',');
+		}
+	}
+	putchar(10);
+	return (0);
 }
--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-    int i = 122;
-    while (i >= 97)
-    {
-        putchar(i);
-        i--;
-    }
-    putchar('\n');
-    return (0);
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
 }
