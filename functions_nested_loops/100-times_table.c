6f6781ae9221fcd97e7bc178008762a63107cc26
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 19:01:00 +0100

    test

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,25 +9,24 @@
 void print_times_table(int n)
 {
 	int a, b, c;
-	int k = 1;
 
 	for (a = 0; a <= n; a++)
 	{
+		_putchar('0');
+
 		for (b = 1; b <= n; b++)
 		{
 			_putchar(44);
 			_putchar(32);
-			c = a * b;
-			while (c / k >= 10)
-				k *= 10;
-			while (k > 0)
-			{
-				putchar('0' + (c / k));
 
-				c %= k;
-				k /= 10;
-			}
-			_putchar(10);
+			c = a * b;
+
+			if (c <= 9)
+				_putchar(32);
+			else if (c <=99 )
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
+		_putchar(10);
 	}
 }
