cebadbc9eb00e705f18211b41702b55520703dde
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 19:32:51 +0100

    test

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -24,12 +24,12 @@ void print_times_table(int n)
 
 			if (c <= 9)
 				_putchar(32);
-			else if (c <= 9)
+			else if (c <= 99)
 				_putchar((c / 10) + '0');
 			else
 			{
 				_putchar((c / 100) + '0');
-				_putchar((c % 10) + '0');
+				_putchar((c % 100) + '0');
 			}
 			_putchar((c % 10) + '0');
 		}
