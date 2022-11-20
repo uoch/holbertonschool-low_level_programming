c4748ca152ef63b024016a1935ca961f8ed01414
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 15:11:01 +0100

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  */
 void print_alphabet(void)
 {
-        int i = 97;
-        while (i <= 122)
-        {
-                _putchar(i);
-                i++;
-        }
-        _putchar(10);
+	int i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
 }
