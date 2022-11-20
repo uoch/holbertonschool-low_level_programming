c2f5118aa18800795e774aa679311ba49eb201b5
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 16:11:16 +0100

    test

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -21,12 +21,15 @@ void print_times_table(int n)
 
 			c = a * b;
 
-			if (c > 9)
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(32);
+			}
+			else
 				_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
-			if (c <= n)
-				_putchar(32);
-			_putchar(10);
 		}
+		_putchar(10);
 	}
 }
