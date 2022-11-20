f9ccc40d856c2be4deaaf96ffac9027d971cfc01
uoch <chaouch.thameurtc61@gmail.com>
2022-11-20 15:09:40 +0100

    test

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 97; j <= 122; j++)
-	{
-		_putchar(j);
-	}
-	_putchar(10);
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(10);
 	}
 }
--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,16 +9,16 @@ int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-        return (1);
-    }
-    else if (c >= 65 && c <= 90)
-    {
 		return (1);
 	}
-    else
+	else if (c >= 65 && c <= 90)
 	{
-    return (0);
+		return (1);
 	}
-    _putchar(10);
-    return (0);
+	else
+	{
+		return (0);
+	}
+	_putchar(10);
+	return (0);
 }
