3344f6d00b31b6d7688d85bb43574f305f35e1b4
uoch <chaouch.thameurtc61@gmail.com>
2022-11-22 20:57:00 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,15 @@
 #include "main.h"
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
