5aed459b1aa3667fe9155190eeaa456e4067f4fa
uoch <chaouch.thameurtc61@gmail.com>
2022-11-24 18:51:22 +0100

    test

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * Return: The number of elements in h.
+ * Return: The number of elements in the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t elements = 0;
+	size_t nodes = 0;
 
-    while (h)
-    {
-        elements++;
-        h = h->next;
-    }
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
 
-    return (elements);
+	return (nodes);
 }
