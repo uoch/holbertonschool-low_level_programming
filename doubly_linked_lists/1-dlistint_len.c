5324c57fbcfef0776748a77a948ab563c3ce0dea
uoch <chaouch.thameurtc61@gmail.com>
2022-11-24 18:47:00 +0100

    test

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t elements = 0;
+    size_t elements = 0;
 
-	while (h)
-	{
-		elements++;
-		h = h->next;
-	}
+    while (h)
+    {
+        elements++;
+        h = h->next;
+    }
 
-	return (elements);
+    return (elements);
 }
