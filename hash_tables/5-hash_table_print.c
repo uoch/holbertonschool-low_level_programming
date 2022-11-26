b002c6b3607b42095e0d0a52e6c1eab732d7ba8c
uoch <chaouch.thameurtc61@gmail.com>
2022-11-26 19:48:48 +0100

    test

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - ints a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int test = 1;
+
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i])
+			{
+				printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				test++;
+				ht->array[i] = ht->array[i]->next;
+			}
+		}
+		i++;
+	}
+	printf("}\n");
+}
