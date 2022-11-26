794f2da1be72df04a4a3b5bc776f7db8f635f917
uoch <chaouch.thameurtc61@gmail.com>
2022-11-26 17:00:18 +0100

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,43 +11,39 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *tmp, *node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+
+	tmp = ht->array[index];
+	while (tmp)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (tmp && strcmp(key, tmp->key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
 			return (1);
 		}
+		tmp = tmp->next;
 	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 	{
-		free(value_copy);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
+	node->key = strdup(key);
+	if (node->key == NULL)
 		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
+	node->value = strdup(value);
+	if (node->value == NULL)
+		return (0);
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
