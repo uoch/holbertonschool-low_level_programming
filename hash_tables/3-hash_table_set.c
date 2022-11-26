4561170cbd84ee9e254b6f3046978433536f5a04
uoch <chaouch.thameurtc61@gmail.com>
2022-11-26 17:22:57 +0100

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,14 +33,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	new = malloc(sizeof(hash_node_t));
-	if (new = NULL)
+	if (new == NULL)
 	{
 		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
 		return (0);
 	}
 	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
-	return (1);
+	return (1)
 }
