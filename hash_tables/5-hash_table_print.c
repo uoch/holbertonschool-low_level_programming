26f5b9ffc79e865da916a7ebd5424440c17e434d
uoch <chaouch.thameurtc61@gmail.com>
2022-11-26 19:49:50 +0100

    test

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,7 +6,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	int test = 1;
+	int test = 0;
 
 	if (!ht)
 		return;
@@ -17,7 +17,8 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			while (ht->array[i])
 			{
-				printf(", ");
+				if (test > 0)
+					printf(", ");
 				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 				test++;
 				ht->array[i] = ht->array[i]->next;
